3b756e695e40e093254b4ccf6d33656984f77646
Ivan Rodriguez E <ivanrenescorcia@gmail.com>
2022-04-29 11:51:15 -0500

    feat: jobList dynamically listed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   jobs = {
-    "first":{
-      "companyName" : "Corporación Empresarial de Salamanca",
+    "first": {
+      "companyName": "Corporación Empresarial de Salamanca",
       "roleTitle": "Software Developer (Intern)",
-      "roleDescription" : "In this internship  for the improvement of the internal student management system, development of new projects in favor of the automation of internal processes",
-      "roleAchievements" : [
+      "roleDescription": "In this internship  for the improvement of the internal student management system, development of new projects in favor of the automation of internal processes",
+      "roleAchievements": [
         "In charge to support university score platform ",
         "Create a students identification software to validate their identity by the security personnel",
         "IT support for administrative personnel"
       ],
-      "jobDuration" : "Jul 2020 - Jan 2021",
+      "jobDuration": "Jul 2020 - Jan 2021",
       "roleSkills": [
         "PHP",
         "jQuery",
@@ -26,17 +26,17 @@ export class AppComponent {
         "Flutter"
       ]
     },
-    "second":{
-      "companyName" : "GudarDevs",
+    "second": {
+      "companyName": "GudarDevs",
       "roleTitle": "Software Developer",
-      "roleDescription" : "Mainly in charge of supporting the projects managed by the company, talking with customers and translation product owner needs to development tasks",
-      "roleAchievements" : [
+      "roleDescription": "Mainly in charge of supporting the projects managed by the company, talking with customers and translation product owner needs to development tasks",
+      "roleAchievements": [
         "In charge of build a system where a bus company can place its available buses and people can place orders to ask for its services",
         "Backend support for online game where you can play with a robot anywhere you be",
         "Create strategy for bot's trading",
         "Backend support for an api where you can analyze pokemon cards"
       ],
-      "jobDuration" : "Feb 2021 - Present",
+      "jobDuration": "Feb 2021 - Feb 2022",
       "roleSkills": [
         "Python",
         "Django",
@@ -45,57 +45,77 @@ export class AppComponent {
         "ReactJS",
         "Docker"
       ]
+    },
+    "third": {
+      "companyName": "Joonik",
+      "roleTitle": "Full-Stack Developer",
+      "roleDescription": "Working on a famous hiring platform, delivering modules for its main features",
+      "roleAchievements": [
+        "Covering full stack tasks like Firebase Cloud Functions with NodeJS, API integrations and Front-end Design",
+      ],
+      "jobDuration": "Feb 2022 - Currently",
+      "roleSkills": [
+        "React",
+        "Redux",
+        "Fuse",
+        "Javascript",
+        "NodeJs",
+        "Firebase",
+      ]
     }
   }
   title = 'Ivan Rodriguez E';
   skills = {
-    "first":{
-      "techName" : "JavaScript",
-      "widthBar" : 90
+    "first": {
+      "techName": "JavaScript",
+      "widthBar": 90
     },
-    "second":{
-      "techName" : "Angular",
-      "widthBar" : 85
+    "second": {
+      "techName": "Angular",
+      "widthBar": 85
     },
-    "third":{
-      "techName" : "HTML/CSS/SASS/LESS",
-      "widthBar" : 70
+    "third": {
+      "techName": "HTML/CSS/SASS/LESS",
+      "widthBar": 70
     },
-    "fourth":{
-      "techName" : "React",
-      "widthBar" : 40
+    "fourth": {
+      "techName": "React",
+      "widthBar": 85
     },
-    "fifth":{
-      "techName" : "Python/Django",
-      "widthBar" : 90
+    "fifth": {
+      "techName": "Python/Django",
+      "widthBar": 90
     }
   }
   education = {
-    "first":{
-      "career" : "Systems Engineer",
-      "university" : "Corporación Universitaria Empresarial de Salamanca · CUES",
-      "year" : "2021 - present"
+    "first": {
+      "career": "Systems Engineer",
+      "university": "Corporación Universitaria Empresarial de Salamanca · CUES",
+      "year": "2021 - present"
     },
-    "second":{
-      "career" : "Software Developer",
-      "university" : "Corporación Universitaria Empresarial de Salamanca · CUES",
-      "year" : "2018 - 2021"
+    "second": {
+      "career": "Software Developer",
+      "university": "Corporación Universitaria Empresarial de Salamanca · CUES",
+      "year": "2018 - 2021"
     },
   }
   awards = {
-    "first":{
-      "awardTitle" : "Python Barranquilla",
-      "awardDescription" : "3th place in the python league tournament with the presentation 'Some curiosities about Django'"
+    "first": {
+      "awardTitle": "Python Barranquilla",
+      "awardDescription": "3th place in the python league tournament with the presentation 'Some curiosities about Django'"
     }
   }
   languaje = {
-    "first":{
-      "languaje" : "Spanish",
-      "level" : "(Native)"
+    "first": {
+      "languaje": "Spanish",
+      "level": "(Native)"
     },
-    "second":{
-      "languaje" : "English",
-      "level" : "(Professional)"
+    "second": {
+      "languaje": "English",
+      "level": "(Professional)"
     },
   }
+
+  jobList: Array<any> = Object.entries(this.jobs).reverse();
+
 }
